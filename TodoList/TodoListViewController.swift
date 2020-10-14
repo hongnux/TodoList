a1f63c4276341a51f3aeeaf6278b145b721735de
hongnux <hom2955@gmail.com>
2020-10-14 18:29:24 +0900

    TodoList Add 구현

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -18,7 +18,6 @@ class TodoListViewController: UIViewController {
     // TODO: TodoViewModel 만들기
     let todoListViewModel = TodoViewModel()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -45,6 +44,13 @@ class TodoListViewController: UIViewController {
         // TODO: Todo 태스크 추가
         // add task to view model
         // and tableview reload or update
+        
+        guard let detail = inputTextField.text, detail.isEmpty == false else { return }
+        let todo = TodoManager.shared.createTodo(detail: detail, isToday: isTodayButton.isSelected)
+        todoListViewModel.addTodo(todo)
+        collectionView.reloadData()
+        inputTextField.text = ""
+        isTodayButton.isSelected = false
     }
     
     // TODO: BG 탭했을때, 키보드 내려오게 하기
@@ -98,9 +104,21 @@ extension TodoListViewController: UICollectionViewDataSource {
             todo = todoListViewModel.upcompingTodos[indexPath.item]
         }
         cell.updateUI(todo: todo)
+        
         // TODO: todo 를 이용해서 updateUI
         // TODO: doneButtonHandler 작성
         // TODO: deleteButtonHandler 작성
+        
+        cell.doneButtonTapHandler = { isDone in
+            todo.isDone = isDone
+            self.todoListViewModel.updateTodo(todo)
+            self.collectionView.reloadData()
+        }
+        
+        cell.deleteButtonTapHandler = {
+            self.todoListViewModel.deleteTodo(todo)
+            self.collectionView.reloadData()
+        }
         return cell
     }
     
